3aec7a056dc9813d0afd93d6d3f3d6cb3eff6500
Alexey Surgaev <Alex.surgaev@gmail.com>
2020-03-11 18:58:13 +0300

    Final Commit *FIXED* REALY :)

--- Perpetual Calendar/ViewModels/AppViewModelValidator.cs
+++ Perpetual Calendar/ViewModels/AppViewModelValidator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Intrinsics.X86;
 using FluentValidation;
 using FluentValidation.Validators;
 
@@ -11,23 +12,32 @@ namespace Perpetual_Calendar.ViewModels
             //RuleFor(vm => vm.Day).InclusiveBetween(1, 31);
             RuleFor(vm => vm.Month).InclusiveBetween(1, 12);
             RuleFor(vm => vm.Year).InclusiveBetween(1, 9999);
-            RuleFor(vm => vm.Day).Must(LeapDecember);
+            RuleFor(vm => vm.Day).Must(DayValidator);
         }
 
-        private static Boolean LeapDecember(AppViewModel vm, Int32 day,PropertyValidatorContext context)
+        private static Boolean DayValidator(AppViewModel vm, Int32 day,PropertyValidatorContext context)
         {
-            if (vm.Month != 2)
+            switch (vm.Month)
             {
-                context.Rule.MessageBuilder = s => "День должен быть от 1 до 31";
-                return day >= 1 && day <= 31;
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    context.Rule.MessageBuilder = s => "День должен быть от 1 до 31";
+                    return day >= 1 && day <= 31;
+                case 2 when DateTime.IsLeapYear(vm.Year):
+                    context.Rule.MessageBuilder = s => "Високосный год. День должен быть от 1 до 29";
+                    return day >= 1 && day <= 29;
+                case 2:
+                    context.Rule.MessageBuilder = s => "День должен быть от 1 до 28";
+                    return day >= 1 && day <= 28;
+                default:
+                    context.Rule.MessageBuilder = s => "День должен быть от 1 до 30";
+                    return day >= 1 && day <= 30;
             }
-            if (DateTime.IsLeapYear(vm.Year))
-            {
-                context.Rule.MessageBuilder = s => "Високосный год. День должен быть от 1 до 29";
-                return day >= 1 && day <= 29;
-            }
-            context.Rule.MessageBuilder = s => "Февраль. День должен быть от 1 до 28";
-            return day >= 1 && day <= 28;
         }
     }
 }
